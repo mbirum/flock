d53d528d18efcff188fad42a961b67dfc47d1191
Matt Birum <birum18@gmail.com>
2023-12-10 14:24:27 -0500

    initial commit

---
+++ Flock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flock
+//
+//  Created by Matt Birum on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flock/FlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlockApp.swift
+//  Flock
+//
+//  Created by Matt Birum on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
